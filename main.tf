40a039c38c8b697d2efe23b50c32c56783093ab0
supriya <bsupriya1835@gmail.com>
2023-04-06 12:29:19 +0530

    Added

--- main.tf
+++ main.tf
@@ -76,7 +76,7 @@ resource "azurerm_linux_virtual_machine" "main" {
   ]
   admin_ssh_key {
     username   = "playbook"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("path")
   }
   os_disk {
     caching              = "ReadWrite"
